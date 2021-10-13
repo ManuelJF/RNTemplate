39299b117e2053dbf5b31bcbf826cfaac1304b02
Luis Juarez <luismanueljf@gmail.com>
2021-10-12 21:34:27 -0500

    create scheme and targets for iOS and Flavors for staging and production

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,12 +1,21 @@
+import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import { Provider } from 'react-redux'
+import { enableScreens } from 'react-native-screens';
 import { store } from './redux/store'
-import Home from './screens/Home';
- 
+import AppStack from './routes/AppNavigator';
+import Config from 'react-native-config'
+
+console.log('ENV VARIABLES:', Config)
+
+enableScreens()
+
 const App = () => {
   return (
     <Provider store={store}>
-      <Home />
+      <NavigationContainer>
+        <AppStack />
+      </NavigationContainer>
     </Provider>
   );
 };
