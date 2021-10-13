03fdbfbd9cfd217a94b9e2a93b8144ab74d78023
Luis Juarez <luismanueljf@gmail.com>
2021-10-12 21:33:39 -0500

    add script to create env files after install

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 #!/usr/bin/env node
 
-console.log("This is post init script");
+var fs = require('fs');
+ 
+console.log('CREATING ENV STAGING FILE:')
+fs.writeFile('.env.staging', 'ENV=staging\n', function (err) {
+  if (err) throw err
+  console.log('Env Staging is created successfully.')
+})
+
+console.log('CREATING ENV PROD FILE:')
+fs.writeFile('.env', 'ENV=production\n', function (err) {
+  if (err) throw err
+  console.log('Env Production is created successfully.')
+})
